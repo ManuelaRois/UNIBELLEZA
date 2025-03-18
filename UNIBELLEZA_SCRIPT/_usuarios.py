17f7c59fe67b940c554032bbdc96a8cfeeeb0d31
ManuelaRois <rrengifomanuela@gmail.com>
2025-03-18 16:56:38 -0500

    Update _usuarios.py

--- UNIBELLEZA_SCRIPT/_usuarios.py
+++ UNIBELLEZA_SCRIPT/_usuarios.py
@@ -1,12 +1,12 @@
-# Diccionario para almacenar usuarios
-usuarios = {}
-
-def registrar_usuario():
+# REGISTRAR USUARIO
+def registrar_usuario(usuarios):
     nombre = input("Ingrese su nombre: ")
     correo = input("Ingrese su correo electrónico: ")
+    telefono = input("Ingrese su número de teléfono: ")
+    direccion = input("Ingrese su dirección: ")
 
     if correo in usuarios:
-        print(f"⚠️ El correo '{correo}' ya está registrado.")
+        print(f"El correo '{correo}' ya está registrado.")
     else:
-        usuarios[correo] = nombre
-        print(f"✅ Usuario '{nombre}' registrado correctamente.")
+        usuarios[correo] = {"nombre": nombre, "telefono": telefono, "direccion": direccion}
+        print(f"Usuario '{nombre}' registrado correctamente.")
